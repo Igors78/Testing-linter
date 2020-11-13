991e8125dfe76d975f331496a00229f4387ebf85
Igors78 <oleinikovsigors@gmail.com>
2020-11-13 20:31:58 +0200

    Line length length test added

--- logic/checkerrors.rb
+++ logic/checkerrors.rb
@@ -1,4 +1,6 @@
 require_relative '../logic/raisingerrors'
+require_relative '../logic/lint'
+
 class CheckErrors
   include RaisingErrors
   def initialize(str, indx)
@@ -7,7 +9,12 @@ class CheckErrors
   end
 
   def check_whitespaces
-    splitted_str = @str.split(' ')
-    raise_an_error(1, @indx) if @str.size != splitted_str.join(' ').size
+    raise_an_error(1, @indx) if @str[@str.size - 2] == ' '
   end
+
+  def check_length
+    raise_an_error(2, @indx) if @str.size > 118
+  end
+
+  def check_new_line; end
 end
--- logic/raisingerrors.rb
+++ logic/raisingerrors.rb
@@ -1,8 +1,12 @@
 module RaisingErrors
-  def raise_an_error(code, index)
+  def raise_an_error(code, index = 0)
     case code
     when 1
-      puts "Line #{index + 1} excessive whitespace(s) detected"
+      puts "Line #{index + 1} trailing whitespace(s) detected"
+    when 2
+      puts "Line #{index + 1} exceeds recommended length"
+    when 3
+      puts 'File should end with a new line'
     end
   end
 end
