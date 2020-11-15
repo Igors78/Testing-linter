909ea529d2fa6fb016230aa2805c0be102176bd9
Igors78 <oleinikovsigors@gmail.com>
2020-11-15 22:02:38 +0200

    Errors to fake added

--- data/fake.rb
+++ data/fake.rb
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
-sum = 1 + 2
+sum = 1 + 2 
 a = 1
-=======
-sum = 1 + 2 |
-      a = 1
->>>>>>> 8fd40c288ef06c20fc82e335ca0579fa2633a0de
 b = 2
 1 > 2; puts 'Hi'
-[1, 2, 3].each { |e| puts e }
+[1, 2, 3].each { |e| puts e } 
 
 some arg.other
-[1, 2, 3].length
+[1, 2, 3]}.length
 
-!array.include?(element)
+! array.include?(element)
